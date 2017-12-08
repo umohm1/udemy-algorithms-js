526a1aa7098c6d33219bbbecba34be22a89f987d
Mary <mumoh@mozilla.com>
2017-12-08 11:07:03 -0500

    Add reverseWords

---
+++ src/reverseWords.js
@@ -0,0 +1,15 @@
+function reverseWords(string) {
+  let wordsArr = string.split(' ');
+  let reversedWordsArr = [];
+
+  wordsArr.forEach(word => {
+    let reversedWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    }
+    reversedWordsArr.push(reversedWord);
+  });
+  return reversedWordsArr.join(' ');
+}
+
+reverseWords('Coding JavaScript');
